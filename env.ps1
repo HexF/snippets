c8d4910163a2a4c9bb5ee9b857c62b509139dcb9
Thomas <iamthomasnz@gmail.com>
2019-11-23 23:06:40 +1300

    Import .env files into powershell environment vars

--- env.ps1
+++ env.ps1
@@ -1,2 +1,2 @@
-#Run this snippet in powershell to import from the local .env file all the keys and values
+#Run this snippet in powershell to import from the local .env file all the keys and values 
 Select-String '^([^=]*)=(.*)' .\.env | ForEach-Object {Set-Item -path ('env:' + $_.Matches.Groups[1].Value) -value $_.Matches.Groups[2].Value}
